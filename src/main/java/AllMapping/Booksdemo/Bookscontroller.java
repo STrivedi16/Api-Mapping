6bb2f0ea2a827dd693aa4c42d32a7be56ef5ea93
STrivedi16 <shubhamtrivedi@nimapinfotech.com>
2023-01-02 16:50:35 +0530

    All Mapping

--- src/main/java/AllMapping/Booksdemo/Bookscontroller.java
+++ src/main/java/AllMapping/Booksdemo/Bookscontroller.java
@@ -2,10 +2,14 @@ package AllMapping.Booksdemo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,10 +28,40 @@ public class Bookscontroller {
 
 	}
 
+	@GetMapping("/books/{id}")
+	public Booksinfo Booksinfo(@PathVariable("id") int id) {
+		Optional<Booksinfo> book = br.findById(id);
+
+		return book.get();
+	}
+
 	@PostMapping("/Addbook")
 	public Booksinfo addbook(@RequestBody Booksinfo bok) {
 
 		return br.save(bok);
+
+	}
+
+	@DeleteMapping("books/{id}")
+	public String Deletebook(@PathVariable("id") int bid) {
+
+		br.deleteById(bid);
+
+		return "your Data has delete";
+
+	}
+
+	@PutMapping("bookupdate/{id}")
+	public Booksinfo updatebook(@RequestBody Booksinfo book, @PathVariable("id") int bid) {
+
+		if (book.getBokid() == bid) {
+			book.setName("The key of Success");
+			book.setAuthor("Shubham Trivedi");
+
+			return br.save(book);
+		} else {
+			return book;
+		}
 
 	}
 
